ba6c0f43e8b37a1127471711a7cf9e4d4ca99c11
Mariko Aburai <aburmari@gmail.com>
2018-11-01 18:25:51 +0900

    削除・完了時の処理機能完成

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,55 @@
 let removeIcon ='<i class="fas fa-trash"></i>';
-let doneIcone ='<i class="far fa-check-circle"></i>';
+let doneIcon ='<i class="far fa-check-circle"></i>';
 
 
 
 
 let add = document.getElementById('add');
 add.addEventListener('click', function(){
-    console.log(1);
     let taskName = document.getElementById('task').value;
-    console.log(taskName);
+// 入力タスクを変数に入れてから、タスク入力欄のユーザーが入れた入力を消す。
+    document.getElementById('task').value = '';
 
 //タスク名を入れたliタグをnotyetに追加する
-
     let li = document.createElement('li');
     li.append(taskName);
-    console.log(li);
+    document.getElementById('not-yet').appendChild(li);
 
+//ボタンを表示させる場所
+    let buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+    li.appendChild(buttons);
+
+//削除ボタンをつける
+    let remove = document.createElement('button');
+    remove.classList.add('remove');
+    remove.innerHTML = removeIcon;
+    buttons.appendChild(remove);
+
+//完了ボタンをつける
+    let done = document.createElement('button');
+    done.classList.add('done');
+    done.innerHTML = doneIcon;
+    buttons.appendChild(done);
+
+
+//削除ボタンが押されたら行う処理
+    remove.addEventListener('click', removeTask);
+
+//完了ボタンが押されたらdoneセクションへ移動する処理
+    done.addEventListener('click', doneTask);
 
 })
+
+
+// -----------------関数---------------------
+//削除ボタンが押されたら行う処理
+function removeTask(){
+        let task = this.parentNode.parentNode;
+        task.remove();
+    }
+//完了ボタンが押されたらdoneセクションへ移動する処理
+function doneTask(){
+        let task = this.parentNode.parentNode;
+        document.getElementById('done').appendChild(task);
+    }
